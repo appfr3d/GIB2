3bad7bfdb4e8d743c7db362b05d5bfc1ed5e1d7f
DESKTOP-306INQD\Sigmund <sigmund.mestad@tekonologiporten.no>
2020-01-30 08:18:39 +0100

    Init app

---
+++ geoMATikk-app/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView 
+        style={styles.mapStyle} 
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        // provider={PROVIDER_GOOGLE}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    ...StyleSheet.absoluteFill,
+    // height: 400,
+    borderColor: 'red',
+    borderWidth: 2
+  },
+});
