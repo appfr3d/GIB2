3d7bfa042d9a53f246c359db9ac3a813c7318c85
DESKTOP-306INQD\Sigmund <sigmund.mestad@tekonologiporten.no>
2020-02-03 16:51:46 +0100

    Refaktorert TopMenu

--- geoMATikk-app/App.js
+++ geoMATikk-app/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TextInput, StatusBar } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import TopMenu from './components/TopMenu';
 
 const marker = {
   latlng: {
@@ -24,16 +23,10 @@ export default function App() {
           latitudeDelta: 0.0372,
           longitudeDelta: 0.0271,
         }}
-        // provider={PROVIDER_GOOGLE}
       >
         <Marker coordinate={marker.latlng} title={marker.title} description={marker.description} />
       </MapView>
-      <View style={styles.topMenu}>
-        <View style={styles.searchView}>
-          <TextInput placeholder="Search" style={styles.searchInput} />
-          <Ionicons name="md-search" size={32} />
-        </View>
-      </View>
+      <TopMenu />
     </View>
   );
 }
@@ -47,29 +40,5 @@ const styles = StyleSheet.create({
   },
   mapStyle: {
     ...StyleSheet.absoluteFill,
-  },
-  topMenu: {
-    position: 'absolute',
-    top: StatusBar.currentHeight + 10,
-    left: 10,
-    borderWidth: 1,
-    borderColor: 'red',
-  },
-  searchView: {
-    // position: 'absolute',
-    // top: StatusBar.currentHeight + 10,
-    // left: 10,
-    width: '50%',
-    height: 40,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 5,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  searchInput: {
-    width: '80%',
-    marginLeft: 5,
   },
 });
