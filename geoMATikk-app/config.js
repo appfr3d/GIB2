92a40a5270a1e9eb06b4d3dd847e4cb4f9f0174a
Alfred Lieth Årøe <alfred.aroe97@gmail.com>
2020-04-29 17:41:56 +0200

    Lagt til herokul linkt til databasen

--- geoMATikk-app/config.js
+++ geoMATikk-app/config.js
@@ -1,10 +1,10 @@
 const config = {
-<<<<<<< HEAD
-  apiDomain: 'http://c2859ca5.ngrok.io',
-=======
-  apiDomain: 'https://886533db.ngrok.io',
->>>>>>> 1396bf4ebf19b18cf4fba53f1782c435f6c99656
+  apiDomain: 'http://geomatikk.herokuapp.com',
   authStorageKey: '@geoMATikk:auth', // Used to save auth object in async storage
 };
+
+// Heroku credentials:
+// mail: alfredla@stud.ntnu.no
+// pass: geoMATikk123!
 
 export default config;
